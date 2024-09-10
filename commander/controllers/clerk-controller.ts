86d6c11df595ea0e7012d5e61e4e37437c16cbb2
Raj Varsani <varasaniraj5@gmail.com>
2024-09-11 04:40:14 +0530

    refactor: Improve user creation logic in clerk-controller.ts

--- commander/controllers/clerk-controller.ts
+++ commander/controllers/clerk-controller.ts
@@ -5,13 +5,17 @@ import { ClerkUserData } from "../types/clerk";
 export const handleUserCreation = async (req: Request, res: Response) => {
   try {
     const userData = req.body.data as ClerkUserData;
-    const userDoc = await User.create({
-      email: userData.email_addresses[0].email_address,
-      firstName: userData.first_name,
-      lastName: userData.last_name,
-      clerkId: userData.id,
-      avatar: userData.image_url,
-    });
+    const userDoc = await User.findOneAndUpdate(
+      { email: userData.email_addresses[0].email_address },
+      {
+        email: userData.email_addresses[0].email_address,
+        firstName: userData.first_name,
+        lastName: userData.last_name,
+        clerkId: userData.id,
+        avatar: userData.image_url,
+      },
+      { upsert: true, new: true }
+    );
 
     console.log("USER CREATED", userDoc._id);
 
